51a6f1d00680e37f4f92b6d894254dcfe65e0311
HeZX <hzx0910@163.com>
2020-01-14 14:46:43 +0800

    测试PreferenceFragment

---
+++ projects/sample/source/sample-plugin/sample-app-lib/src/main/java/com/tencent/shadow/sample/plugin/app/lib/usecases/fragment/MyPreferenceFragment.java
@@ -0,0 +1,9 @@
+package com.tencent.shadow.sample.plugin.app.lib.usecases.fragment;
+
+import android.preference.PreferenceFragment;
+
+/**
+ * Created by HeZX on 2020-01-14.
+ */
+public class MyPreferenceFragment extends PreferenceFragment {
+}
